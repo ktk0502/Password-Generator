12c1e76e156471b8ae8d9e0602464713c5718131
Kartikay Pratap Singh <kartikay0502@gmail.com>
2024-04-10 14:31:09 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 style={{color:'white'}}>Password Generator</h1>
+      <div style={{color:'white'}} className="container">ma</div>
+    </>
+  )
+}
+
+export default App
